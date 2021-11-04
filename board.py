90c40ae6bc73c3b4c998caf1b0df4431b780060a
Chavelier <maniakorto124@gmail.com>
2021-11-04 13:21:12 +0100

    pas mal de modifs

--- board.py
+++ board.py
@@ -42,6 +42,26 @@ class Board:
     not_ab_file = ~(a_file | b_file)
     not_gh_file = ~(g_file | h_file)
 
+    bishop_relevant_bits = [
+    6, 5, 5, 5, 5, 5, 5, 6,
+    5, 5, 5, 5, 5, 5, 5, 5,
+    5, 5, 7, 7, 7, 7, 5, 5,
+    5, 5, 7, 9, 9, 7, 5, 5,
+    5, 5, 7, 9, 9, 7, 5, 5,
+    5, 5, 7, 7, 7, 7, 5, 5,
+    5, 5, 5, 5, 5, 5, 5, 5,
+    6, 5, 5, 5, 5, 5, 5, 6
+    ]
+    rook_relevant_bits = [
+    12, 11, 11, 11, 11, 11, 11, 12,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    11, 10, 10, 10, 10, 10, 10, 11,
+    12, 11, 11, 11, 11, 11, 11, 12
+    ]
 
     def __init__(self):
         self.init()
@@ -70,39 +90,58 @@ class Board:
 
 
     # fonctions sur les bits -----------------------------------------------------------------------
-
-    def set_bit(self,bitboard,case):
+    @staticmethod
+    def set_bit(bitboard,case):
         """ U64 , int -> U64
         renvoi le bitboard auquel on a mis un 1 sur la case """
         return bitboard | (U64(1) << U64(case))
-    def pop_bit(self,bitboard,case):
+    @staticmethod
+    def pop_bit(bitboard,case):
         """ U64 , int -> U64
         renvoi le bitboard auquel on a mis un 0 sur la case """
         return bitboard & ~(U64(1) << U64(case))
-    def switch_bit(self,bitboard,case):
+    @staticmethod
+    def switch_bit(bitboard,case):
         """ U64 , int -> U64
         renvoi le bitboard auquel on a change le bit de la case """
         return bitboard ^ (U64(1) << U64(case))
-    def get_bit(self,bitboard,case):
+    @staticmethod
+    def get_bit(bitboard,case):
         """ U64 , int -> U64
         renvoi le bit de la case demandee du bitboard """
         return bitboard & (U64(1) << U64(case))
-    def bit_state(self,bitboard,case):
-        """ U64 , int -> bool
-        renvoi l'etat actuel de la case du bitboard """
-        return self.get_bit(bitboard,case) != 0
+
+    @staticmethod
+    def count_bit(bitboard): #verifier efficacite (static inline equivalent)
+        """ U64 -> int
+        renvoi le nombre de bit du bitboard """ # TODO: on peut ameliorer la fonction
+        bb = bitboard
+        count = 0
+        while bb:
+            count += 1
+            bb = bb & (bb-U64(1)) # enleve le bit le moins signifiant
+        return count
+    @staticmethod
+    def ls1b_index(bitboard): #verifier efficacite (static inline equivalent)
+        """ U64 -> int
+        renvoi l'index du bit le moins signifiant """ # TODO: on peut ameliorer la fonction
+        return Board.count_bit((bitboard & -bitboard)-U64(1))
+
 
 
     # affichage / debug ------------------------------------------------------------------------------------
     def case_str2int(self,txt):
         val = self.coord.index(txt)
         return val
+    def case_int2str(self,nb):
+        val = self.coord[nb]
+        return val
     def print_bb(self,bitboard):
         print("val : %s \n"%bitboard)
         for i in range(8):
             ligne = str(8-i)+"   "
             for j in range(8):
-                txt = str(int(self.bit_state(bitboard,8*i+j)))
+                txt = str(int(Board.get_bit(bitboard,8*i+j)!=0))
                 ligne += txt+" "
             print(ligne)
         print("\n    a b c d e f g h\n")
@@ -111,19 +150,28 @@ class Board:
     # INITIALISATION DES ATTAQUES ###########################################################################
 
     def init_leaper_attack(self):
-        self.pawn_attack = {}
+        self.pawn_attack = [[],[]]
+        self.knight_attack = []
         for i in range(64):
-            self.pawn_attack['blanc'].append(self.mask_pawn_attack(i,False))
-            self.pawn_attack['noir'].append(self.mask_pawn_attack(i,True))
+
+            # on initialise les attaques de pion
+            self.pawn_attack[0].append(self.mask_pawn_attack(i,False))
+            self.pawn_attack[1].append(self.mask_pawn_attack(i,True))
+
+            #on initialise les attaques de cavalier
+            self.knight_attack.append(self.mask_knight_attack(i))
+
+            #on initialise les attaques du roi
+            self.knight_attack.append(self.mask_king_attack(i))
 
     # ATTAQUES DES PIECES ###############################################################################
 
     def mask_pawn_attack(self,case,side):
         """ int , bool -> U64
-        renvoi le bitboard de l'attaque du pion sur la case """
+        renvoi le bitboard de l'attaque du pion sur la case "case" """
 
         attack = U64(0)
-        bb = self.set_bit(U64(0),case) #position du pion en bitboard
+        bb = Board.set_bit(U64(0),case) #position du pion en bitboard
 
         if not side:
             attack = ((bb & self.not_h_file) >> U64(7)) | ((bb & self.not_a_file) >> U64(9))
@@ -131,3 +179,150 @@ class Board:
             attack = ((bb & self.not_a_file) << U64(7)) | ((bb & self.not_h_file) << U64(9))
 
         return attack
+
+    def mask_knight_attack(self,case):
+        """ int , bool -> U64
+        renvoi le bitboard de l'attaque du cavalier sur la case "case" """
+
+        attack = U64(0)
+        bb = Board.set_bit(U64(0),case) #position du cavalier en bitboard
+
+        attack = (bb & self.not_h_file) >> U64(15)
+        attack = attack | (bb & self.not_a_file) >> U64(17)
+        attack = attack | (bb & self.not_ab_file) >> U64(10)
+        attack = attack | (bb & self.not_gh_file) >> U64(6)
+        attack = attack | (bb & self.not_a_file) << U64(15)
+        attack = attack | (bb & self.not_h_file) << U64(17)
+        attack = attack | (bb & self.not_gh_file) << U64(10)
+        attack = attack | (bb & self.not_ab_file) << U64(6)
+
+        return attack
+
+    def mask_king_attack(self,case):
+        """ int , bool -> U64
+        renvoi le bitboard de l'attaque du roi sur la case "case" """
+
+        attack = U64(0)
+        bb = Board.set_bit(U64(0),case) #position du roi en bitboard
+
+        attack = bb >> U64(8) | bb << U64(8)
+        if bb & self.not_a_file: attack = attack | bb >> U64(1) | bb >> U64(9) | bb << U64(7)
+        if bb & self.not_h_file: attack = attack | bb << U64(1) | bb << U64(9) | bb >> U64(7)
+
+        return attack
+
+    def mask_bishop_attack(self,case):
+        """ int , bool -> U64
+        renvoi le bitboard de l'attaque du fou sur la case "case" """
+
+        attack = U64(0)
+
+        rank,file = case//8, case%8 # ligne et colonne de la pieces
+        for i in range(1,min(7-rank,7-file)):
+            r,f = rank+i,file+i
+            attack = attack | U64(1) << U64(r*8 + f)
+        for i in range(1,min(rank,file)):
+            r,f = rank-i,file-i
+            attack = attack | U64(1) << U64(r*8 + f)
+        for i in range(1,min(7-rank,file)):
+            r,f = rank+i,file-i
+            attack = attack | U64(1) << U64(r*8 + f)
+        for i in range(1,min(rank,7-file)):
+            r,f = rank-i,file+i
+            attack = attack | U64(1) << U64(r*8 + f)
+
+        return attack
+
+    def mask_rook_attack(self,case):
+        """ int , bool -> U64
+        renvoi le bitboard de l'attaque de la tour sur la case "case" """
+
+        attack = U64(0)
+
+        rank,file = case//8, case%8 # ligne et colonne de la pieces
+        for i in range(1,7-rank):
+            r,f = rank+i,file
+            attack = attack | U64(1) << U64(r*8 + f)
+        for i in range(1,rank):
+            r,f = rank-i,file
+            attack = attack | U64(1) << U64(r*8 + f)
+        for i in range(1,7-file):
+            r,f = rank,file+i
+            attack = attack | U64(1) << U64(r*8 + f)
+        for i in range(1,file):
+            r,f = rank,file-i
+            attack = attack | U64(1) << U64(r*8 + f)
+
+        return attack
+
+    # PIECES GLISSANTES (sliding pieces) "on the fly" #####################################
+
+    def bishop_attack_on_the_fly(self,case,block):
+        attack = U64(0)
+
+        rank,file = case//8, case%8 # ligne et colonne de la pieces
+        for i in range(1,min(8-rank,8-file)):
+            r,f = rank+i,file+i
+            b = U64(1) << U64(r*8 + f)
+            attack = attack | b
+            if b & block: break #on sors de la boucle si on rencontre un bloqueur
+        for i in range(1,min(rank+1,file+1)):
+            r,f = rank-i,file-i
+            b = U64(1) << U64(r*8 + f)
+            attack = attack | b
+            if b & block: break #on sors de la boucle si on rencontre un bloqueur
+        for i in range(1,min(8-rank,file+1)):
+            r,f = rank+i,file-i
+            b = U64(1) << U64(r*8 + f)
+            attack = attack | b
+            if b & block: break #on sors de la boucle si on rencontre un bloqueur
+        for i in range(1,min(rank+1,8-file)):
+            r,f = rank-i,file+i
+            b = U64(1) << U64(r*8 + f)
+            attack = attack | b
+            if b & block: break #on sors de la boucle si on rencontre un bloqueur
+
+        return attack
+
+    def rook_attack_on_the_fly(self,case,block):
+        attack = U64(0)
+
+        rank,file = case//8, case%8 # ligne et colonne de la pieces
+        for i in range(1,8-rank):
+            r,f = rank+i,file
+            b = U64(1) << U64(r*8 + f)
+            attack = attack | b
+            if b & block: break #on sors de la boucle si on rencontre un bloqueur
+        for i in range(1,rank+1):
+            r,f = rank-i,file
+            b = U64(1) << U64(r*8 + f)
+            attack = attack | b
+            if b & block: break #on sors de la boucle si on rencontre un bloqueur
+        for i in range(1,8-file):
+            r,f = rank,file+i
+            b = U64(1) << U64(r*8 + f)
+            attack = attack | b
+            if b & block: break #on sors de la boucle si on rencontre un bloqueur
+        for i in range(1,file+1):
+            r,f = rank,file-i
+            b = U64(1) << U64(r*8 + f)
+            attack = attack | b
+            if b & block: break #on sors de la boucle si on rencontre un bloqueur
+
+        return attack
+
+    # MAGIC bitboard function
+
+    def set_occupancies(self,index, bits_in_mask, attack_mask):
+        """pas encore trop compris ce que ça fait..."""
+        occupancy = U64(0)
+        attack_map = attack_mask
+
+        for i in range(bits_in_mask):
+            square = Board.ls1b_index(attack_map)
+            attack_map = Board.pop_bit(attack_map,square)
+
+            if U64(index) & (U64(1)<<U64(i)):
+                occupancy = occupancy | (U64(1)<<U64(square))
+
+        return occupancy
--- main.py
+++ main.py
@@ -2,11 +2,25 @@ from board import *
 
 B = Board()
 # B.print_bb(B.pop_bit(B.Pw,51))
-case = B.case_str2int('e1')
-B.print_bb(B.mask_pawn_attack(case,True))
+case = B.case_str2int('c7')
+block = U64(0)
+block = B.set_bit(block,5)
+block = B.set_bit(block,13)
+block = B.set_bit(block,34)
 
+atk_bb = B.mask_bishop_attack(B.case_str2int("e4"))
+# for i in range(4096):
+#     B.print_bb(B.set_occupancies(i,B.count_bit(atk_bb),atk_bb))
+#     txt = input()
 
 # v = U64(0)
 # for i in range(8):
 #         v = B.set_bit(v,i*8+6)
 # B.print_bb(B.not_ab_file)
+
+for r in range(8):
+    line = ""
+    for f in range(8):
+        case = 8*r + f
+        line += str(B.count_bit(B.mask_rook_attack(case))) + ", "
+    print(line)
