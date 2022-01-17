732b08d23730f893e772849ecc2581d68490d2ce
Chavelier <58261721+Chavelier@users.noreply.github.com>
2022-01-17 11:37:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ from board import *
 
 B = Board()
 
-c = B.case_str2int
 B.print_board(False)
-# occ = U64(0)
-# occ = B.set_bit(occ,c("e4"))
-# occ = B.set_bit(occ,c("d4"))
-# occ = B.set_bit(occ,c("e5"))
-# occ = B.set_bit(occ,c("d5"))
-# B.print_bb(occ)
-#
-#
-# for case in range(64):
-#     # B.print_bb(B.rook_attack_on_the_fly(case,occ))
-#     B.print_bb(B.get_rook_attack(case, occ))
-#     a = input("...")
-#     if a == "q":
-#         break
+occ = U64(0)
+occ = B.set_bit(occ,B.E4)
+occ = B.set_bit(occ,B.D4)
+occ = B.set_bit(occ,B.E5)
+occ = B.set_bit(occ,B.D5)
+B.print_bb(occ)
+
+
+for case in range(64):
+    # B.print_bb(B.rook_attack_on_the_fly(case,occ))
+    B.print_bb(B.get_bishop_attack(case, occ))
+    a = input("...")
+    if a == "q":
+        break
+
